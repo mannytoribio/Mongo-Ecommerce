0d9af281c858951f76a80776772b5a1b2c6463be
Juan Yepes <yepes.j@hotmail.com>
2021-10-08 16:22:30 -0400

    updates

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv' //npm i dotenv
 import express from 'express'
 import { ObjectId } from 'mongodb'
+import { findCarts, findCartsById, updateCart } from './src/cart.js'
 // import functions from 'firebase-functions'
 import { createCustomer, findCustomers, findCustomersById, updateCutomer } from './src/customers.js'
 import { createInventory, findInventory } from './src/inventory.js'
 import { createTransactions, findTransactions } from './src/transactions.js'
+import { } from './src/cart'
 
 dotenv.config()
 
@@ -59,6 +61,39 @@ app.post('/inventory', async (req, res) => createInventory)
 
 app.get('/transactions:id', async (req, res) => findTransactions)
 app.post('/transactions', async (req, res) => createTransactions)
+
+app.get('/cart/:id', async (req, res) => {
+  try {
+    const id = new ObjectId(req.params.id)
+    let cart = await findcartById(req.body)
+    res.status(200).send(cart)
+  } catch (err) {
+    res.status(500).send(err)
+    console.log(err)
+  }
+})
+
+app.patch('/cart/:id', async (req, res) => {
+  try {
+    const id = new ObjectId(req.params.id)
+    let cart = await updateCart(req.body)
+    res.status(200).send(cart)
+  } catch (err) {
+    res.status(500).send(err)
+    console.log(err)
+  }
+})
+
+app.get('/cart/', async (req, res) => {
+  try { 
+    let customer = await findCarts(req.body)
+    res.status(200).send(cart)
+  } catch (err) {
+    res.status(500).send(err)
+    console.log(err)
+  }
+})
+
 
 
 app.listen(3000, () => console.log('listening on port 3000'))
