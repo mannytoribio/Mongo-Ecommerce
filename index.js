b5b9f98b32a2f5ba0f3bae0090cf8a5e3252aa2b
Manny Toribio <manny.mtoribio@gmail.com>
2021-10-08 15:31:35 -0400

    added inventory endpoints

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 import { ObjectId } from 'mongodb'
 // import functions from 'firebase-functions'
 import { createCustomer, findCustomers, findCustomersById, updateCutomer } from './src/customers.js'
-import { createInventory, findInventory } from './src/inventory.js'
+import { createInventory, findInventory, findInventoryById, updateInventory, deleteInventory } from './src/inventory.js'
 import { createTransactions, findTransactions } from './src/transactions.js'
 
 dotenv.config()
@@ -25,7 +25,6 @@ app.get('/customers/:id', async (req, res) => {
 
 app.patch('/customers/:id', async (req, res) => {
   try {
-    const id = new ObjectId(req.params.id)
     let customer = await updateCutomer(req.body)
     res.status(200).send(customer)
   } catch (err) {
@@ -54,7 +53,28 @@ app.post('/customers', async (req, res) => {
   }
 })
 
-app.get('/inventory/:id', async (req, res) => findInventory)
+app.patch('/inventory/:id', async (req, res) => {
+  try {
+    let inventory = updateInventory(id)
+    res.status(200).send(inventory)
+  } catch (err) {
+    res.status(500).send(err)
+    console.log(err)
+  }
+})
+
+
+app.get('/inventory/:id', async (req, res) => {
+  try {
+    let inventory = await findInventory(req.body)
+    res.status(200).send(inventory)
+  } catch (err) {
+    res.status(500).send(err)
+    console.log(err)
+  }
+})
+
+
 app.post('/inventory', async (req, res) => createInventory)
 
 app.get('/transactions:id', async (req, res) => findTransactions)
